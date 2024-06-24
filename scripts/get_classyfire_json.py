84f049d8bc4971d510506cd3cd4cf9c5ebeff052
Kozo Nishida <kozo.nishida@gmail.com>
2024-06-24 16:20:23 +0900

    Update get_classyfire_json.py

--- scripts/get_classyfire_json.py
+++ scripts/get_classyfire_json.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import json
 
 df = pd.read_csv("inchikey4accession.csv")
 
@@ -7,7 +8,8 @@ for i in df["InChIKey"].unique():
   if isinstance(i, str):
     #r = requests.get('http://classyfire.wishartlab.com/entities/' + i + '.json')
     r = requests.get('https://cfb.fiehnlab.ucdavis.edu/entities/' + i)
-    with open(i + '.json', 'w') as f:
-      print(json.dumps(parsed, indent=4), file=f)
+    if i == r.json()['_id']:
+      with open(i + '.json', 'w') as f:
+        print(json.dumps(r.json(), indent=4), file=f)
   else:
     print(i)
