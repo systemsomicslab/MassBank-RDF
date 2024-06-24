56db3f918535b2c87544942f4e5be0227dc1714b
Kozo Nishida <kozo.nishida@gmail.com>
2024-06-24 14:45:11 +0900

    Update get_classyfire_json.py

--- scripts/get_classyfire_json.py
+++ scripts/get_classyfire_json.py
@@ -5,9 +5,9 @@ df = pd.read_csv("inchikey4accession.csv")
 
 for i in df["InChIKey"].unique():
   if isinstance(i, str):
-    r = requests.get('http://classyfire.wishartlab.com/entities/' + i + '.json')  
+    #r = requests.get('http://classyfire.wishartlab.com/entities/' + i + '.json')
+    r = requests.get('https://cfb.fiehnlab.ucdavis.edu/entities/' + i)
     with open(i + '.json', 'w') as f:
       print(json.dumps(parsed, indent=4), file=f)
   else:
     print(i)
-    
